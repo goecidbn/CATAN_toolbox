ea323ae47ead466faab83d13f96acb60ba274ff5
woxxel <alexander.wollex.schmidt@googlemail.com>
2026-07-26 10:54:25 +0200

    further fixes to OS/Win specific GUI testing... #2

--- tests/test_gui_smoke.py
+++ tests/test_gui_smoke.py
@@ -1,17 +1,28 @@
+import os
+import sys
 import pytest
 
 
 @pytest.fixture(scope="session")
 def qapp():
+    if sys.platform.startswith("linux"):
+        os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
+
+    print("Creating QApplication...")
+    print("platform:", sys.platform)
+    print("QT_QPA_PLATFORM =", os.environ.get("QT_QPA_PLATFORM"))
+
     from PySide6.QtWidgets import QApplication
 
     app = QApplication.instance()
 
+    print("Existing app:", app)
+
     if app is None:
+        print("Constructing QApplication...")
         app = QApplication(["catan-test"])
-
+        print("Done.")
     return app
-
 
 @pytest.mark.gui
 def test_gui_application_imports(qapp):
